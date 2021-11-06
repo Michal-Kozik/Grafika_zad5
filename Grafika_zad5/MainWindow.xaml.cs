32cdaaf690d2cbf1dce54147aaa2548645469838
Michal Kozik <michal.kozikowski99@gmail.com>
2021-11-06 16:06:10 +0100

    Binaryzacja ręczna i iteratywna.

--- Grafika_zad5/MainWindow.xaml.cs
+++ Grafika_zad5/MainWindow.xaml.cs
@@ -48,6 +48,21 @@ namespace Grafika_zad5
             resultHistogram.Children.Clear();
         }
 
+        private bool IsInputCorrect()
+        {
+            if (!Int32.TryParse(inputTreshhold.Text, out int result))
+            {
+                MessageBox.Show("Podane dane są niepoprawne.", "Niepoprawne dane", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (result < 0 || result > 255)
+            {
+                MessageBox.Show("Podany zakres powinien być od 0 do 255.", "Zły zakres", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
         // Dla czarnobialych.
         private void DrawSourceHistogram(byte[] pixelBuffer)
         {
@@ -208,6 +223,157 @@ namespace Grafika_zad5
             imgResult.Source = ConvertBitmapToImageSource(imgResultBitmap);
         }
 
+        private void CustomBinarization(object sender, RoutedEventArgs e)
+        {
+            if (!ImageExist())
+            {
+                MessageBox.Show("Nie załadowano obrazka!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (!IsInputCorrect())
+            {
+                return;
+            }
+
+            // Zrodlo.
+            Bitmap imgSourceBitmap = ConvertImgToBitmap(imgSource);
+            BitmapData sourceBitmapData = imgSourceBitmap.LockBits(new Rectangle(0, 0, imgSourceBitmap.Width, imgSourceBitmap.Height),
+                                                             ImageLockMode.ReadOnly,
+                                                             System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceBitmapData.Stride * sourceBitmapData.Height];
+            Marshal.Copy(sourceBitmapData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+            imgSourceBitmap.UnlockBits(sourceBitmapData);
+
+            ClearSourceHistogram();
+            ClearResultHistogram();
+            byte treshold = Convert.ToByte(inputTreshhold.Text);
+            // Dla czarno-bialego obrazka.
+            for (int i = 0; i + 4 < pixelBuffer.Length; i += 4)
+            {
+                if (pixelBuffer[i] >= treshold)
+                {
+                    pixelBuffer[i] = 255;
+                    pixelBuffer[i + 1] = 255;
+                    pixelBuffer[i + 2] = 255;
+                }
+                else
+                {
+                    pixelBuffer[i] = 0;
+                    pixelBuffer[i + 1] = 0;
+                    pixelBuffer[i + 2] = 0;
+                }
+            }
+
+            // Rezultat.
+            Bitmap imgResultBitmap = new Bitmap(imgSourceBitmap.Width, imgSourceBitmap.Height);
+            BitmapData resultBitmapData = imgResultBitmap.LockBits(new Rectangle(0, 0, imgResultBitmap.Width, imgResultBitmap.Height),
+                                                                   ImageLockMode.WriteOnly,
+                                                                   System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(pixelBuffer, 0, resultBitmapData.Scan0, pixelBuffer.Length);
+            imgResultBitmap.UnlockBits(resultBitmapData);
+            imgResult.Source = ConvertBitmapToImageSource(imgResultBitmap);
+        }
+
+        private void IterativeBinarization(object sender, RoutedEventArgs e)
+        {
+            if (!ImageExist())
+            {
+                MessageBox.Show("Nie załadowano obrazka!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Zrodlo.
+            Bitmap imgSourceBitmap = ConvertImgToBitmap(imgSource);
+            BitmapData sourceBitmapData = imgSourceBitmap.LockBits(new Rectangle(0, 0, imgSourceBitmap.Width, imgSourceBitmap.Height),
+                                                             ImageLockMode.ReadOnly,
+                                                             System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceBitmapData.Stride * sourceBitmapData.Height];
+            Marshal.Copy(sourceBitmapData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+            imgSourceBitmap.UnlockBits(sourceBitmapData);
+
+            ClearSourceHistogram();
+            ClearResultHistogram();
+            byte thirdByte = 0;
+            byte lastByte = 0;
+            int objectBytes = 0;
+            int backgroundBytes = 0;
+            int objectSum = 0;
+            int backgroundSum = 0;
+            // Dla czarno-bialego obrazka.
+            // Pierwsza iteracja.
+            for (int i = 4; i + 4 < pixelBuffer.Length; i += 4)
+            {
+                if (i + 4 > pixelBuffer.Length)
+                {
+                    lastByte = pixelBuffer[i];
+                    thirdByte = pixelBuffer[i - sourceBitmapData.Stride + 4];
+                }
+                else
+                {
+                    objectSum += pixelBuffer[i];
+                    objectBytes++;
+                }
+            }
+            double averageObjectBrightness = objectSum / (double)objectBytes;
+            double averageBackgroundBrightness = (pixelBuffer[0] + pixelBuffer[sourceBitmapData.Stride - 4] + thirdByte + lastByte) / 2.0;
+            double treshold = (averageObjectBrightness + averageBackgroundBrightness) / 2.0;
+            // Kolejne Iteracje.
+            objectBytes = 0;
+            backgroundBytes = 0;
+            objectSum = 0;
+            backgroundSum = 0;
+            for (int n = 0; n < sliderIteration.Value; n++)
+            {
+                for (int i = 0; i + 4 < pixelBuffer.Length; i += 4)
+                {
+                    if (pixelBuffer[i] >= treshold)
+                    {
+                        objectBytes++;
+                        objectSum += pixelBuffer[i];
+                    }
+                    else
+                    {
+                        backgroundBytes++;
+                        backgroundSum += pixelBuffer[i];
+                    }
+                }
+                averageObjectBrightness = objectSum / (double)objectBytes;
+                averageBackgroundBrightness = backgroundSum / (double)backgroundBytes;
+                treshold = (averageObjectBrightness + averageBackgroundBrightness) / 2.0;
+            }
+            // Binaryzacja.
+            for (int i = 0; i + 4 < pixelBuffer.Length; i += 4)
+            {
+                if (pixelBuffer[i] >= treshold)
+                {
+                    pixelBuffer[i] = 255;
+                    pixelBuffer[i + 1] = 255;
+                    pixelBuffer[i + 2] = 255;
+                }
+                else
+                {
+                    pixelBuffer[i] = 0;
+                    pixelBuffer[i + 1] = 0;
+                    pixelBuffer[i + 2] = 0;
+                }
+            }
+
+            // Rezultat.
+            Bitmap imgResultBitmap = new Bitmap(imgSourceBitmap.Width, imgSourceBitmap.Height);
+            BitmapData resultBitmapData = imgResultBitmap.LockBits(new Rectangle(0, 0, imgResultBitmap.Width, imgResultBitmap.Height),
+                                                                   ImageLockMode.WriteOnly,
+                                                                   System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(pixelBuffer, 0, resultBitmapData.Scan0, pixelBuffer.Length);
+            imgResultBitmap.UnlockBits(resultBitmapData);
+            imgResult.Source = ConvertBitmapToImageSource(imgResultBitmap);
+        }
+
         private Bitmap ConvertImgToBitmap(System.Windows.Controls.Image source)
         {
             RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)source.ActualWidth, (int)source.ActualHeight, 96.0, 96.0, PixelFormats.Pbgra32);
@@ -237,5 +403,7 @@ namespace Grafika_zad5
             bitmapImage.EndInit();
             return bitmapImage;
         }
+
+        
     }
 }
